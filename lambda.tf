f9d5895f9bfc02c88a6c9e3f199e504856f21be0
Adam Dobrawy <adam@getselectstar.com>
2022-06-14 09:48:23 +0200

    Bump python version

--- lambda.tf
+++ lambda.tf
@@ -4,7 +4,7 @@ module "lambda" {
   function_name = var.name
   description   = "Manages ASG instance alarms"
   handler       = "lambda.lambda_handler"
-  runtime       = "python3.6"
+  runtime       = "python3.9"
   layers        = var.lambda_layers
   timeout       = 30
 
