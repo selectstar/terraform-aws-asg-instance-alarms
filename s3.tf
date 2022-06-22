e6bf526b0e1f2bed6c719f8b36d0302cb4f8f834
Adam Dobrawy <adam@getselectstar.com>
2022-06-23 00:11:03 +0200

    Block public access in bucket

--- s3.tf
+++ s3.tf
@@ -11,3 +11,12 @@ resource "aws_s3_bucket" "alarm_templates" {
 
   tags = var.bucket-tags
 }
+
+resource "aws_s3_bucket_public_access_block" "alarm_templates" {
+  bucket = aws_s3_bucket.alarm_templates.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
